21fc761b987502e2ffa7a7c1f9c29bf162318b05
yoku0825 <yoku0825@gmail.com>
2018-09-07 11:59:10 +0900

    Now mikasafabric needs WITH GRANT OPTION in managed-mysqld

--- Dockerfile/mysql_fabric_aware/init.sh
+++ Dockerfile/mysql_fabric_aware/init.sh
@@ -3,7 +3,7 @@
 service mysqld start
 mysql -p$(awk '/root@localhost/{print $NF}' /var/log/mysqld.log) --connect-expired-password -e "ALTER USER user() IDENTIFIED BY ''"
 mysql -uroot -e "CREATE USER root; GRANT ALL ON *.* TO root WITH GRANT OPTION;"
-mysql -uroot -e "CREATE USER fabric; GRANT ALL ON *.* TO fabric;"
+mysql -uroot -e "CREATE USER fabric; GRANT ALL ON *.* TO fabric WITH GRANT OPTION;"
 mysql -uroot -e "CREATE USER ap; GRANT ALL ON ap.* TO ap;"
 mysql -uroot -e "RESET MASTER"
 service mysqld stop
